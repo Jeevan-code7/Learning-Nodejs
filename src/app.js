b24b3fdf9cfa583c4caf8a11940c631d044215b0
jeevan <jk29102023@gmail.com>
2026-02-21 14:07:10 +0530

    find all users in database

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const app = express();
-
+require("mongoose");
 app.use(express.json());
 
 const connectDB = require("./config/data");
 const User = require("./models/userModel");
 
-app.post("/signup", async (req, res) => {
-  const userData = new User(req.body);
+app.get("/user", async (req, res) => {
+  const userData = req.body.email;
+
   try {
-    await userData.save();
-    res.status(200).send("data added successfully in database");
-  } catch (err) {
-    console.log("error occured", err);
+    const user = await User.find({});
+    console.log(user);
+    res.status(200).send("All data fetched successfully");
+  } catch (error) {
+    console.log("error", error);
   }
 });
+
+// app.post("/signup", async (req, res) => {
+//   const userData = new User(req.body);
+//   try {
+//     await userData.save();
+//     res.status(200).send("data added successfully in database");
+//   } catch (err) {
+//     console.log("error occured", err);
+//   }
+// });
 
 connectDB()
   .then(() => {
